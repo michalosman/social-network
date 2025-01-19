bbb913906c5140896a7662a91663742c9d1035bd
Michał Osman <40360401+michalosman@users.noreply.github.com>
2025-01-19 14:50:40 +0100

    feat: add github and linkedin links

--- src/components/HomeMenu.tsx
+++ src/components/HomeMenu.tsx
@@ -1,7 +1,7 @@
 import { Button, Flex, Icon, Text } from '@chakra-ui/react'
 import { AiFillShop } from 'react-icons/ai'
 import { BiTimer } from 'react-icons/bi'
-import { FaUserFriends } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaUserFriends } from 'react-icons/fa'
 import { MdGroups, MdKeyboardArrowDown } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
@@ -52,6 +52,30 @@ function HomeMenu() {
           See more
         </Text>
       </Button>
+      <a
+        href="https://github.com/michalosman"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button w="100%" mt={12} size="lg" variant="left">
+          <Icon as={FaGithub} color="#060606" fontSize="36px" />
+          <Text ml={2} fontSize="15px">
+            GitHub
+          </Text>
+        </Button>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/michalosman/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button w="100%" size="lg" variant="left">
+          <Icon as={FaLinkedin} color="#0F82BD" fontSize="36px" />
+          <Text ml={2} fontSize="15px">
+            LinkedIn
+          </Text>
+        </Button>
+      </a>
     </Flex>
   )
 }
